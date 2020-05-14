82643d09c83c1017d94d8e9ded07687625212b5b
jabbiati <jabbiati@yahoo.com>
2020-05-14 15:13:08 -0400

    first change set

---
+++ precache-manifest.5c4738f3f6c6f667f801cd33e4bc4c64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931ffaa3e35052fe7527cd4e37a5ed28",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bd227c4a312486b85eb4",
+    "url": "./static/css/main.d18b0eba.chunk.css"
+  },
+  {
+    "revision": "e69cf0c2c403fdffc5b7",
+    "url": "./static/js/2.94bb3674.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "./static/js/2.94bb3674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd227c4a312486b85eb4",
+    "url": "./static/js/main.c6e5746f.chunk.js"
+  },
+  {
+    "revision": "c8bd3a4250e58bd039ab",
+    "url": "./static/js/runtime-main.13383c87.js"
+  }
+]);
